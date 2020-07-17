4ce921350196ea4188037a644129ebffe51be0b5
Tcaprioli <Capriolityler@gmail.com>
2020-07-17 18:42:35 -0400

    removed unneeded hook

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {IoIosArrowDropdown} from 'react-icons/io'
 import {NavLink} from 'react-router-dom';
 import '../App.css';
 
 
 export default()=>{
-  const [selected,useSelected] = useState(false)
   return (
     <div className="background">
       <div id="portrait"></div>
@@ -13,26 +12,16 @@ export default()=>{
         <h1 className="text">Tyler Caprioli</h1>
         <h2 className="text">Software Engineer / Full Stack Web Developer</h2>
         
-        {selected? 
+       
         
         <span id='arrow-con'>
           <NavLink
           to='/about'
           >
-            <IoIosArrowDropdown id='arrow' style={{color: "gray"}}  onMouseLeave={()=>{useSelected(false)}}/>
+            <IoIosArrowDropdown id='arrow' />
           </NavLink>
         </span >
-        :
-        <span id='arrow-con' >
-          <NavLink
-          to='/about'
-          >
-
-          <IoIosArrowDropdown id='arrow' onMouseEnter={()=>{useSelected(true)}}/>
-          </NavLink>
-        </span >
-        
-        }
+       
         </div>
     </div>
   );
