09ffc95c734c69ebeff8588d193bdc390dbc8814
Tcaprioli <Capriolityler@gmail.com>
2020-07-16 22:53:07 -0400

    added homepage with navbar and content components

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -10,8 +10,7 @@ function Homepage() {
         <div className='content'>
             <h1>About Me</h1>
             <img id='bout-pic' src={require('../media/army.png')}/>
-            <p >whaddup i'm watching hot ones and I still kinda want to kill myself but not really. honestly I just really want to
-              watch some porn rn
+            <p> placeholder text
             </p>
 
             
