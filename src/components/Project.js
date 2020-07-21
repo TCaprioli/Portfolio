a5bcee508bf34171a02e3a68774c999b2095878f
Tcaprioli <Capriolityler@gmail.com>
2020-07-21 19:32:40 -0400

    updated demo desc

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,7 +8,7 @@ export default (props)=>{
     <h4>GitHub: <a href={props.gitFront} rel='noopener noreferrer' target='_blank'>Front-end</a> /
     <a href={props.gitBack} rel='noopener noreferrer' target='_blank'> Back-end</a></h4>
     <h4>Click here for <a href={props.demo} rel='noopener noreferrer' target='_blank'>Demo</a></h4>
-
+    {props.title === 'AppTrack'? <h4>Click here for a <a href='https://www.loom.com/share/7b6662d06a724eaba0fce829ad8d60e1' rel='noopener noreferrer' target='_blank'>Video Demo</a></h4>: null}
     <img className='project-img' src={props.img} alt={props.imgAlt}/> 
 
     <p className='text-box'>{props.description}</p>
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -14,7 +14,7 @@ export default()=>{
 
     <Project title='AppTrack' gitFront='https://github.com/TCaprioli/AppTrack-Frontend' gitBack='https://github.com/TCaprioli/AppTrack-Backend'
     demo='https://apptrack.netlify.app/'  img={AppTrack} imgAlt='AppTrack main page'
-    description='Apptrack is a job application tracker built with React, Redux, and Ruby on Rails. This application allows a user to upload PDF resumes, log job applications, and organize the applications with folders.'/>
+    description='Apptrack is a job application tracker built with React, Redux, and Ruby on Rails. This application allows a user to upload PDF resumes, log job applications, and organize the applications with folders. /n Live demo only logs applications --  See video demo for full features.'/>
 
     <Project title='Sonata-React' gitFront='https://github.com/TCaprioli/Sonata-React-Frontend' gitBack='https://github.com/TCaprioli/Sonata/tree/master/Backend'
     demo='http://sonata-react.herokuapp.com' img={Sonata} imgAlt='sonata main page'
