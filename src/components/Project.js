c1700e4da1c4378881d62676708aa03cbf2cd0b0
Tcaprioli <Capriolityler@gmail.com>
2020-07-21 19:40:42 -0400

    added video and live demo

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,10 +5,10 @@ export default (props)=>{
     <>
     <h2 className='project-title'>{props.title}</h2>
 
-    <h4>GitHub: <a href={props.gitFront} rel='noopener noreferrer' target='_blank'>Front-end</a> /
-    <a href={props.gitBack} rel='noopener noreferrer' target='_blank'> Back-end</a></h4>
     <h4>Click here for <a href={props.demo} rel='noopener noreferrer' target='_blank'>Demo</a></h4>
     {props.title === 'AppTrack'? <h4>Click here for a <a href='https://www.loom.com/share/7b6662d06a724eaba0fce829ad8d60e1' rel='noopener noreferrer' target='_blank'>Video Demo</a></h4>: null}
+    <h4>GitHub: <a href={props.gitFront} rel='noopener noreferrer' target='_blank'>Front-end</a> /
+    <a href={props.gitBack} rel='noopener noreferrer' target='_blank'> Back-end</a></h4>
     <img className='project-img' src={props.img} alt={props.imgAlt}/> 
 
     <p className='text-box'>{props.description}</p>
